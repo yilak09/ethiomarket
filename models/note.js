a86254cf5301191c93f718b19bddc370537c6fbd
yilak09 <isaakayele@gmail.com>
2022-02-06 12:42:48 -0600

    create and pagenot found added

--- models/note.js
+++ models/note.js
@@ -2,6 +2,8 @@ const mongoose = require("mongoose");
 const noteSchema = new mongoose.Schema({
     title: String,
     description: String
+},{
+    timestamps:true
 });
 
 const Note = mongoose.model('Note',noteSchema);
--- routes/note.js
+++ routes/note.js
@@ -21,4 +21,6 @@ router.post('/notes', async (req, res) => {
   }
 })
 
+
+
 module.exports = router;
